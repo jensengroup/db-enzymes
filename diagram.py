74cdda370e31e6b0497065709b78234cd230eff0
Jimmy Charnley Kromann <jimmy@charnley.dk>
2018-07-21 19:22:07 +0200

    added energy diagram module

--- diagram.py
+++ diagram.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
-from cycler import cycler
+from itertools import izip, count
 
 def look_nice(ax, xaxis, xticks):
 
@@ -31,26 +31,22 @@ def add_line(ax, x1, x2, y1, y2, color='g', ls='--'):
     return
 
 
-def add_energy_path(ax, energies, color):
+def add_energy_path(ax, energies, color=None, label=None, epsilon=0.1):
 
+    xs = np.array([0 - epsilon, 0 + epsilon])
+    ys = np.array([0, 0])
 
+    # plot the first energy level and set color
+    p = plt.plot(xs, ys+energies[0], color=color, label=label)
+    if not color: color = p[0].get_color()
+
+    for i, energy in enumerate(energies[1:]):
+        plt.plot(xs+i+1, ys+energy, color=color)
+
+    for i, energy_1, energy_2 in izip(count(), energies[:-1], energies[1:]):
+        add_line(ax, i+epsilon, i+1-epsilon, energy_1, energy_2, color=color)
 
     return
-
-def next_color():
-
-    color_cycle = plt.rcParams['axes.prop_cycle']
-    # colors = color_cycle.by_key()['color']
-
-    for color in color_cycle:
-        yield color['color']
-
-
-    # for color in colors:
-    #     yield color
-    #
-    # yield "#111111"
-
 
 
 if __name__ == "__main__":
@@ -58,48 +54,16 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-
-    color =  next(next_color())
-
-    print color
-
-    path = [0, 20, -10]
     names = ["R", "TS", "P"]
-    xaxis = [1, 2, 3]
+    xaxis = list(range(len(names)))
 
-    epsilon = 0.1
+    path = [0, 23.2, -8.1]
+    add_energy_path(ax, path, label="path 1")
 
-    xs = np.array([1-epsilon, 1+epsilon])
-    ys = np.array([0, 0])
-
-
-    ts = 21.67
-    p = -8.4343
-
-    plt.plot(xs, ys, "-", color=color, label="path 2")
-    plt.plot(xs+1, ys+ts, "-", color=color)
-    plt.plot(xs+2, ys+p, "-", color=color)
-    add_line(ax, 1+epsilon, 2-epsilon, 0, ts, color=color)
-    add_line(ax, 2+epsilon, 3-epsilon, ts, p, color=color)
-
-    ts = 18.67
-    p = -8.4343
-
-    color = next(next_color())
-    color = next(next_color())
-    color = next(next_color())
-
-    print color
-    color='r'
-
-    plt.plot(xs, ys, "-", color=color, label="path 2")
-    plt.plot(xs+1, ys+ts, "-", color=color)
-    plt.plot(xs+2, ys+p, "-", color=color)
-    add_line(ax, 1+epsilon, 2-epsilon, 0, ts, color=color)
-    add_line(ax, 2+epsilon, 3-epsilon, ts, p, color=color)
+    path = [0, 10.2, -5.4]
+    add_energy_path(ax, path, label="path 2")
 
     look_nice(ax, xaxis, names)
-
 
     if True:
         leg = ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
